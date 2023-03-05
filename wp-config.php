0a318f9d6fe71ad1cb22001cdf2c72187a3968ad
Ediekkhan <eddychristantus@gmail.com>
2023-03-05 17:28:49 +0100

    Final task

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'egry' );
 define( 'DB_USER', 'Kanex' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'mboti1Dem' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
