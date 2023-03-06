e5c3929a88706e573741168415254f8957b7b90c
Ediekkhan <eddychristantus@gmail.com>
2023-03-06 15:16:00 +0100

    okay

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'egry' );
 
 /** Database username */
-define( 'DB_USER', 'Kanex' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'mboti1Dem' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
